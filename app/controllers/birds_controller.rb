882300a26f5aa0fc011b87a8b87ee225ab189510
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-11 23:14:04 +0000

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,13 @@
 class BirdsController < ApplicationController
+
+  def index
+    birds = Bird.all
+    render json: BirdSerializer.new(birds)
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json BirdSerializer.new(bird)
+  end
+
 end
